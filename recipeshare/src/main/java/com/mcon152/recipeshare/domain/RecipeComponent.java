43554f8d5478f00a56ccab40deebe187cad3a35c
arobinso9 <arobinso9@student.touro.edu>
2025-12-25 17:33:51 -0500

    RS1F2-237

--- recipeshare/src/main/java/com/mcon152/recipeshare/domain/RecipeComponent.java
+++ recipeshare/src/main/java/com/mcon152/recipeshare/domain/RecipeComponent.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface RecipeComponent {
 
-    // This will return the name of the recipe or the meal plan
+    //   This will return the name of the recipe or the meal plan
     String getName();
 
     // This will return the final list of recipes (flattened)
