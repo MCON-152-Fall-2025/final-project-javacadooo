8aad0f15fb180d4419a11ab1793d1a9f9cfcc622
arobinso9 <arobinso9@student.touro.edu>
2025-12-25 17:28:53 -0500

    RS1F2-234

---
+++ recipeshare/src/main/java/com/mcon152/recipeshare/domain/SingleRecipeComponent.java
@@ -0,0 +1,22 @@
+package com.mcon152.recipeshare.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SingleRecipeComponent implements RecipeComponent {
+    private Recipe recipe;
+
+    public SingleRecipeComponent(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    @Override
+    public String getName() {
+        return recipe.getName();
+    }
+
+    @Override
+    public List<Recipe> getRecipes() {
+        return Collections.singletonList(this.recipe);
+    }
+}
