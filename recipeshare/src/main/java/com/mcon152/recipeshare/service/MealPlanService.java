ceb6f49b906b9a437d94203fcb8920dc0100b9f3
tehillasebrow <tehillasebrow123@gmail.com>
2026-01-01 15:31:57 -0500

    Refactor imports in MealPlanService.java

--- recipeshare/src/main/java/com/mcon152/recipeshare/service/MealPlanService.java
+++ recipeshare/src/main/java/com/mcon152/recipeshare/service/MealPlanService.java
@@ -5,8 +5,8 @@ import com.mcon152.recipeshare.domain.Recipe;
 import com.mcon152.recipeshare.repository.MealPlanRepository;
 import com.mcon152.recipeshare.repository.RecipeRepository;
 // These require Aviva's work:
-import com.mcon152.recipeshare.pattern.MealPlanComponent;
-import com.mcon152.recipeshare.pattern.SingleRecipeComponent;
+import com.mcon152.recipeshare.domain.MealPlanComponent;
+import com.mcon152.recipeshare.domain.SingleRecipeComponent;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
